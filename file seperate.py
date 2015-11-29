12e8f9c112d77745f1c7fd97f816ccf10f9d0c65
zozazola <qthjianghao@gmail.com>
2015-11-30 00:38:29 +0300

    seperate any number of gases

--- file seperate.py
+++ file seperate.py
@@ -4,23 +4,33 @@ Created on Sun Nov 29 10:44:48 2015
 
 @author: JIANGH
 """
-#import numpy as np
 
-# filename = input("filename: ")
-filename = "CO2 VT.txt"
-count_line = -3
+filename = input("filename: ")
+#filename = "CO2 VT.txt"
+count_line = 0
 count = []
 fhand = open(filename)
 for line in fhand:
     count_line += 1
     if line.startswith("#ManPress"):
         count.append(count_line)
-        print("count: " + str(count_line))
-        
+        print("count: " + str(count_line))        
 print(count)
-#data = np.genfromtxt(filename)
-#for i in len(count):
-#    newdata = data[count[i]:count[i+1]]
-#    export_filename = str(i) + ".txt"
-#    np.savetxt(export_filename, newdata)
+
+file = open(filename, 'r')
+data = file.readlines()
+for i in range(len(count)):
+    filename_new = str(i + 1) + ".txt"
+    newfile = open(filename_new, 'w')
+    newfile.write("gas")    
+    newfile.write(str(i + 1))
+    newfile.write("\n")
+    if i == len(count) -1:
+        newdata = data[count[i]-1:]
+    else:
+        newdata = data[count[i]-1:count[i+1]-1]
+    for line in newdata: 
+        newfile.write(line)
+    newfile.close()
+    
     
