f8ba412d58a4fc7cd6b1942502f359d4f7300e52
zozazola <qthjianghao@gmail.com>
2015-11-30 14:30:36 +0300

    code refactoring with function

--- 3flex kinetics general.py
+++ 3flex kinetics general.py
@@ -49,48 +49,30 @@ if myfile == "CO2 VT.txt":
     x2=data[188028 :,3]
     x2=x2/1000/60
     y2=data[188028 :,22]
+    
+def figure_plot(x, y, myfile_label, myfile_fig):
+    fig = plt.figure()
+    fig.set_size_inches(6, 4)
+    plot, = plt.plot(x,y,'ko', label=myfile_label)
+    
+    xmin, xmax = x.min(), x.max()
+    ymin, ymax = y.min(), y.max()
+    dx, dy = (xmax-xmin)*0.1, (ymax-ymin)*0.1
+    plt.xlim(xmin-dx, xmax + dx)
+    plt.ylim(ymin-dy, ymax + dy)
+    plt.ylabel('Pressure (torr)')
+    plt.xlabel('time (minutes)')
+    
+    ax = plt.gca()
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    
+    plt.legend(numpoints=1, loc='upper left', frameon=False, title=None)
+    plt.savefig(myfile_fig, format="png", dpi=600)
+    plt.close()
+    
 # plot the first sample    
-
-fig = plt.figure()
-fig.set_size_inches(6, 4)
-plot1, = plt.plot(x1,y1,'ko', label=myfile_label)
-
-xmin1, xmax1 = x1.min(), x1.max()
-ymin1, ymax1 = y1.min(), y1.max()
-dx1, dy1 = (xmax1-xmin1)*0.1, (ymax1-ymin1)*0.1
-plt.xlim(xmin1-dx1, xmax1 + dx1)
-plt.ylim(ymin1-dy1, ymax1 + dy1)
-plt.ylabel('Pressure (torr)')
-plt.xlabel('time (minutes)')
-
-ax = plt.gca()
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
-
-plt.legend(numpoints=1, loc='upper left', frameon=False, title=None)
-plt.savefig(myfile_fig1, format="png", dpi=600)
-plt.close()
-
+figure_plot(x1, y1, myfile_label, myfile_fig1)
 # plot the second sample
-
-fig = plt.figure()
-fig.set_size_inches(6, 4)
-plot2, = plt.plot(x2,y2,'ko', label = myfile_label)
-
-xmin2, xmax2 = x2.min(), x2.max()
-ymin2, ymax2 = y2.min(), y2.max()
-dx2, dy2 = (xmax2-xmin2)*0.1, (ymax2-ymin2)*0.1
-plt.xlim(xmin2-dx2, xmax2 + dx2)
-plt.ylim(ymin2-dy2, ymax2 + dy2)
-
-plt.ylabel('Pressure (torr)')
-plt.xlabel('time (minutes)')
-
-ax = plt.gca()
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
-
-plt.legend(numpoints=1, loc='upper left', frameon=False, title=None)
-plt.savefig(myfile_fig2, format="png", dpi=600)
-plt.close()
+figure_plot(x2, y2, myfile_label, myfile_fig2)
 print("done")
